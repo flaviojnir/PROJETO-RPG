f003b3ac1336812a71650c0e7724ede16bb8300f
flaviojnir <138256597+flaviojnir@users.noreply.github.com>
2023-06-30 18:03:20 -0300

    Add files via upload

---
+++ elfo.java
@@ -0,0 +1,37 @@
+class elfo {
+    private String nome;
+    private int idade;
+    private String altura;
+    public double desl = 10.5;
+
+    public elfo(String nome, int idade, String altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getAltura() {
+        return altura;
+    }
+
+    public void setAltura(String altura) {
+        this.altura = altura;
+    }
+
+}
