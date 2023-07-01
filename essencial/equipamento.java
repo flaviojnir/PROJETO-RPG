95febac5c286cf2523955b774bd1a4e281ffa324
erick <erickbranquinhomachado@gmail.com>
2023-07-01 02:03:47 -0300

    equipamento

--- essencial/equipamento.java
+++ essencial/equipamento.java
@@ -1,5 +1,13 @@
 package essencial;
 
 public class equipamento {
-    
+    String nome;
+    String tipo;
+    int stat;
+
+    public equipamento(String nome,String tipo,int stat){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.stat = stat;
+    }
 }
